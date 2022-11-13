4d4c9528eb54748ac07fd8c77a99b8ce87c1798f
Tejas Patade <tej.patade69@gmail.com>
2022-11-13 16:49:37 +0530

    Day3 Commit-2

--- Day3-MedianDataStream.py
+++ Day3-MedianDataStream.py
@@ -14,23 +14,23 @@ class MedianFinder:
         # Checking if all elements of leftHeap are smaller than those of rightHeap
         # The condition becomes true when lefHeap has value greater tha smallest value in rightHeap which is wrong & thus we shift the value to rightHeap
         if (self.leftHeap and self.rightHeap and (-1 * self.leftHeap[0]) > self.rightHeap[0]):
-            value = -1 * heapq.heappop(self.leftHeap[0])
+            value = -1 * heapq.heappop(self.leftHeap)
             heapq.heappush(self.rightHeap, value)
         
         # Finally, we check if leftHeap and rightHeap are equally balanced in size
         # Check if leftHeap is greater by 2 or more elements than rightHeap
         if len(self.leftHeap) > len(self.rightHeap) + 1:
-            value = -1 * heapq.heappop(self.leftHeap[0])
+            value = -1 * heapq.heappop(self.leftHeap)
             heapq.heappush(self.rightHeap, value)
         # Vice Versa
         if len(self.rightHeap) > len(self.leftHeap) + 1:
-            value = heapq.heappop(self.rightHeap[0])
+            value = heapq.heappop(self.rightHeap)
             heapq.heappush(self.leftHeap, value * -1)
 
     def medianFind(self) -> float:
+        if len(self.leftHeap) > len(self.rightHeap):
+            return -1 * self.leftHeap[0]
         if len(self.leftHeap) < len(self.rightHeap):
             return self.rightHeap[0]
-        if len(self.leftHeap) > len(self.rightHeap):
-            return self.leftHeap[0]
         
-        return (self.leftHeap[0] + self.rightHeap[0]) / 2
+        return (-1 * self.leftHeap[0] + self.rightHeap[0]) / 2
